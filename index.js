26524815eab41ebba368ff804a27b81c254d16cb
OswalB <grafitexpasto@gmail.com>
2024-12-02 22:02:45 -0500

    cuadricula ok, next crear ficha

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let mesa1 = [], mesa2 = []
-const ratio = 30, rowsMax =5, mesa1x=320, mesa1y = 100
+const ratio = 15, rowsMax = 5, mesa1x = 100, mesa1y = 100, margin = 2
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 init();
@@ -8,30 +8,54 @@ init();
 
 
 function init() {
-    mesa1 = [...loadTable(  mesa1x, mesa1y)];
+    mesa1 = [...loadTable(mesa1x, mesa1y)];
     drawMesa(mesa1)
 }
 
-function loadTable(  xx, yy) {
-    const arr = []
-    for (i = 0; i < rowsMax; i++) {
-        yy = yy + (ratio * 1.75);
-        xx = xx-(ratio);
-        for (j = 0; j <= i; j++) {
-            x2 = xx + (ratio*2 * j);
-            arr.push({ xx:x2, yy })
-        }
+function loadTable(x, y) {
+    const
+        arr = [],
+        ww = rowsMax + (margin * 2)
+        ;
+    for (let row = 0; row <= ww; row++) {
+        let rowArray = []
+        let xx = row % 2 === 0 ? x : x - ratio, yy = y;
+        for (let col = 0; col <= ww; col++) {
+            let zone = false;
+            if (row >= margin && row-margin < rowsMax) {
+                center = Math.round((rowsMax + margin) / 2);
+                l = col >= (center -Math.trunc((row-margin)/2 ));
+                h = col <= (center +Math.round((row-margin)/2 ));
+                if (l && h) {
+                    zone = true;
+                    console.log(row,col)
+                }
 
+            }
+            rowArray.push({
+                zone, buzy: false,
+                xx: xx + (ratio * 2 * col*1.02),
+                yy: yy + (ratio * 1.75 * row*1.02)
+            });
+            
+        }
+        arr.push(rowArray);
     }
+
+
     return arr;
 
 }
 
-function drawMesa(mesaArr){
-    
-    mesaArr.forEach(fila => {
-        drawVoidCircle(fila.xx,fila.yy,ratio,'#d6dbdf',2)
-    });
+function drawMesa(mesaArr) {
+    const ww = rowsMax + (margin * 2);
+    for (let row = 0; row <= ww; row++) {
+        for (let col = 0; col <= ww; col++) {
+            const { xx, yy, zone } = mesaArr[col][row];
+            drawVoidCircle(xx, yy, ratio, zone ? 'black' : '#d6dbdf', 1);
+        }
+    }
+
 }
 
 
@@ -39,17 +63,17 @@ function drawMesa(mesaArr){
 
 function drawCircle(x, y, radio, color) {
     ctx.beginPath();
-    ctx.arc(x, y, radio, 0, Math.PI * 2); 
-    ctx.fillStyle = color;              
-    ctx.fill();                         
+    ctx.arc(x, y, radio, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
     ctx.closePath();
 }
 function drawVoidCircle(x, y, radio, colorBorde, grosorBorde) {
     ctx.beginPath();
-    ctx.arc(x, y, radio, 0, Math.PI * 2); 
-    ctx.strokeStyle = colorBorde;        
-    ctx.lineWidth = grosorBorde;         
-    ctx.stroke();                        
+    ctx.arc(x, y, radio, 0, Math.PI * 2);
+    ctx.strokeStyle = colorBorde;
+    ctx.lineWidth = grosorBorde;
+    ctx.stroke();
     ctx.closePath();
 }
 
